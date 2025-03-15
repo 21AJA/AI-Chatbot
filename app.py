73ba76e267671ef2efd0c4705aff152a86cd5216
Ajay Kumar Maurya <ajaykumarmaurya760@gmail.com>
2025-03-16 00:35:26 +0530

    initial commit

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def chat():
         return jsonify({"error": "Internal Server Error", "details": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
